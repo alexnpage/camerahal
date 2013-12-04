my4ndr0id/android_vendor_qcom_android-open

camera: add ION support in native HAL code
Change-Id: Ic303fdb1b23acc38aaa9d14c1a48da8fb47e4c2e

libcamera2/QualcommCameraHardware.h
@@ -21,6 +21,7 @@
 #include <camera/CameraHardwareInterface.h>
 #include <binder/MemoryBase.h>
 #include <binder/MemoryHeapBase.h>
+#include <binder/MemoryHeapIon.h>
 #include <stdint.h>
 #include <ui/Overlay.h>
 
@@ -226,6 +227,35 @@ private:
         sp<QualcommCameraHardware::MMCameraDL> mMMCameraDLRef;
     };
 
+    struct IonPool : public MemPool {
+        IonPool( int ion_heap_id, int flags, int ion_type,
+             int buffer_size, int num_buffers,
+             int frame_size, int cbcr_offset,
+             int yoffset, const char *name);
+    virtual ~IonPool();
+    int mFd;
+    int mIonType;
+    int mCbCrOffset;
+    int myOffset;
+    int mCameraControlFd;
+    uint32_t mAlignedSize;
+    sp<QualcommCameraHardware::MMCameraDL> mMMCameraDLRef;
+    static const char mIonDevName[];
+    };
+#ifdef USE_ION
+    sp<IonPool> mPreviewHeap;
+    sp<IonPool> mYV12Heap;
+    sp<IonPool> mRecordHeap;
+    sp<IonPool> mThumbnailHeap;
+    sp<IonPool> mRawHeap;
+    sp<IonPool> mDisplayHeap;
+    sp<AshmemPool> mJpegHeap;
+    sp<AshmemPool> mStatHeap;
+    sp<AshmemPool> mMetaDataHeap;
+    sp<IonPool> mRawSnapShotPmemHeap;
+    sp<IonPool> mLastPreviewFrameHeap;
+    sp<IonPool> mPostviewHeap;
+#else
     sp<PmemPool> mPreviewHeap;
     sp<PmemPool> mYV12Heap;
     sp<PmemPool> mRecordHeap;
@@ -238,7 +268,7 @@ private:
     sp<PmemPool> mRawSnapShotPmemHeap;
     sp<PmemPool> mLastPreviewFrameHeap;
     sp<PmemPool> mPostviewHeap;
-
+#endif
 
     sp<MMCameraDL> mMMCameraDLRef;
 

alexnpage/camerahal

QualcommCameraHardware.h
@@ -21,6 +21,7 @@
 #include "CameraHardwareInterface.h"
 #include <binder/MemoryBase.h>
 #include <binder/MemoryHeapBase.h>
+#include <binder/MemoryHeapBaseIon.h>
 #include <utils/threads.h>
 #include <stdint.h>
 #include "Overlay.h"
@@ -354,6 +355,34 @@ private:
         sp<QualcommCameraHardware::MMCameraDL> mMMCameraDLRef;
     };
 
+    struct IonPool : public MemPool {
+        IonPool( int ion_heap_id, int flags, int ion_type,
+             int buffer_size, int num_buffers,
+             int frame_size, int cbcr_offset,
+             int yoffset, const char *name);
+        virtual ~IonPool();
+        int mFd;
+        int mIonType;
+        int mCbCrOffset;
+        int myOffset;
+        int mCameraControlFd;
+        uint32_t mAlignedSize;
+        sp<QualcommCameraHardware::MMCameraDL> mMMCameraDLRef;
+        static const char mIonDevName[];
+    };
+
+#ifdef USE_ION
+    sp<IonPool> mPreviewHeap;
+    sp<IonPool> mRecordHeap;
+    sp<IonPool> mThumbnailHeap;
+    sp<IonPool> mRawHeap;
+    sp<IonPool> mDisplayHeap;
+    sp<AshmemPool> mJpegHeap;
+    sp<AshmemPool> mStatHeap;
+    sp<AshmemPool> mMetaDataHeap;
+    sp<IonPool> mRawSnapShotPmemHeap;
+    sp<IonPool> mPostviewHeap;
+#else
     sp<PmemPool> mPreviewHeap;
     sp<PmemPool> mRecordHeap;
     sp<PmemPool> mThumbnailHeap;
@@ -364,7 +393,7 @@ private:
     sp<AshmemPool> mMetaDataHeap;
     sp<PmemPool> mRawSnapShotPmemHeap;
     sp<PmemPool> mPostViewHeap;
-
+#endif
 
     sp<MMCameraDL> mMMCameraDLRef;
 